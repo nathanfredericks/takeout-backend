899c12f257a39b1e61466ba104e5dc1a13aab312
Nathan Fredericks <nathan@fredericks.dev>
2025-03-27 12:19:54 -0300

    Update database environment variables

--- db.py
+++ db.py
@@ -15,12 +15,12 @@ DB_TYPE = os.getenv("DB_TYPE", "sqlite")
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-mysql_user = os.getenv("MYSQL_USER", "root")
-mysql_password = os.getenv("MYSQL_PASSWORD", "")
-mysql_host = os.getenv("MYSQL_HOST", "localhost")
-mysql_port = os.getenv("MYSQL_PORT", "3306")
-mysql_db = os.getenv("MYSQL_DB", "takeout")
-mysql_url = f"mysql+pymysql://{mysql_user}:{mysql_password}@{mysql_host}:{mysql_port}/{mysql_db}"
+mysql_user = os.getenv("MYSQL_USER")
+mysql_password = os.getenv("MYSQL_PASSWORD")
+mysql_host = os.getenv("MYSQL_HOST")
+mysql_port = os.getenv("MYSQL_PORT")
+mysql_database = os.getenv("MYSQL_DATABASE")
+mysql_url = f"mysql+pymysql://{mysql_user}:{mysql_password}@{mysql_host}:{mysql_port}/{mysql_database}"
 
 if DB_TYPE.lower() == "mysql" or DB_TYPE.lower() == "mariadb":
     db_url = mysql_url
